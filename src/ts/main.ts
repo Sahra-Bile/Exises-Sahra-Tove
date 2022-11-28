9d3366b55e40045677fcf551670879530f2e773a
Sahra <sahra.bile134@gmail.com>
2022-11-28 14:28:57 +0100

    hämtat api

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,11 @@
+ import axios from 'axios'; 
 console.log("hej"); 
 const array: string[] = ["Sahra", "Tove", "Jasika", "Sebastian"];
 
  array.push("Liyana");
  console.log(array);
+  
+
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
+  axios.get(url).then();
+
